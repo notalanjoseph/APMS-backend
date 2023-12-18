2c22e1bb3605a299cfafed89fbab0450a6fbcbb9
Alan Joseph <notalanjoseph@gmail.com>
2023-12-18 23:42:57 +0530

    changes

--- maineon.py
+++ maineon.py
@@ -103,9 +103,9 @@ def get_new():
 
 
 
-if __name__ == "__main__":
-    import uvicorn
-    uvicorn.run(app, host="0.0.0.0", port=8080)
+#if __name__ == "__main__":
+#    import uvicorn
+#    uvicorn.run(app, host="0.0.0.0", port=8080)
 
 # Close the cursor and connection
 #cursor.close()
